747fa9e743fea866f027a9ce1828497fc01b0ef2
PMS-GIT005 <main.malinthapsenadheera@gmail.com>
2023-07-05 22:15:58 +0530

    First Commit

---
+++ nvim/init.lua
@@ -0,0 +1,21 @@
+local opt=vim.opt
+
+opt.number= true
+opt.relativenumber= true
+
+require('plugins')
+require('p-null')
+require('p-lsp')
+require('p-lspsaga')
+require('p-tree')
+require('color')
+require('text')
+require('p-lualine')
+require('p-telescope')
+require('p-lspkind')
+require('p-cmp')
+require('p-snip')
+require('style')
+require('other')
+require('base')
+require('maps')
